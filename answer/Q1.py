f5724be13f79c0dcfe8d040368c18b759b801b23
Ryvan <31363123+rvnfdy@users.noreply.github.com>
2023-03-27 12:39:40 +0800

    Update Q1.py

--- answer/Q1.py
+++ answer/Q1.py
@@ -11,15 +11,15 @@ sql = """
 WITH jurong_island_port_info as (SELECT *
 FROM `bigquery-public-data.geo_international_ports.world_port_index` 
 WHERE country = 'SG' AND port_name = 'JURONG ISLAND'
-LIMIT 1 -- safe handling in case of data duplication
+LIMIT 1
 )
 
 SELECT port_name, distance_in_meters
 FROM (SELECT port_name, ST_DISTANCE(ST_GEOGPOINT(port_longitude, port_latitude), ST_GEOGPOINT((select port_longitude from jurong_island_port_info), (select port_latitude from jurong_island_port_info))) as distance_in_meters
 FROM `bigquery-public-data.geo_international_ports.world_port_index`)
-WHERE distance_in_meters > 0 -- to exclude the port used as reference
+WHERE distance_in_meters > 0
 ORDER BY 2
-LIMIT 5)
+LIMIT 5
 """
 
 # Start the query, passing in the extra configuration.
