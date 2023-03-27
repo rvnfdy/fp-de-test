1699585eb22f53a1ca5174676574cdd9263d320e
Ryvan <31363123+rvnfdy@users.noreply.github.com>
2023-03-27 12:37:58 +0800

    Update and rename Q1.sql to Q1.py

--- answer/Q1.py
+++ answer/Q1.py
@@ -1,4 +1,13 @@
-CREATE TABLE fp-de-test.default.five_nearest_ports_to_jurong_island_port as (
+from google.cloud import bigquery
+
+# Construct a BigQuery client object.
+client = bigquery.Client()
+
+table_id = "fp-de-test.default.five_nearest_ports_to_jurong_island_port"
+
+job_config = bigquery.QueryJobConfig(destination=table_id)
+
+sql = """
 WITH jurong_island_port_info as (SELECT *
 FROM `bigquery-public-data.geo_international_ports.world_port_index` 
 WHERE country = 'SG' AND port_name = 'JURONG ISLAND'
@@ -11,3 +20,10 @@ FROM `bigquery-public-data.geo_international_ports.world_port_index`)
 WHERE distance_in_meters > 0 -- to exclude the port used as reference
 ORDER BY 2
 LIMIT 5)
+"""
+
+# Start the query, passing in the extra configuration.
+query_job = client.query(sql, job_config=job_config)  # Make an API request.
+query_job.result()  # Wait for the job to complete.
+
+print("Query results loaded to the table {}".format(table_id))
